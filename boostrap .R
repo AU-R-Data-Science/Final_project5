0134602505e98a6371969c542fca055d61d9545f
mikerlyjo <micmisco@gmail.com>
2022-11-29 22:40:40 -0600

    I update the boostrap for the coeffcient. Please check it.

--- boostrap .R
+++ boostrap .R
@@ -1,21 +1,19 @@
 
-#boostrap confidence interval 
-Boostrap <- function(x, alpha =0.05,R =1000, seed = NULL, ...) {
-  #Calculate the mean
-  x <- x[!is.na(x)]
-  estimate <- mean(x)
-  n <- length(x)
-  boot_mean <- rep(NA, R)
-  for (i in 1:R){
-    x_sampling <-x[sample(1:n, replace = TRUE)]
-    boot_mean[i] <- mean(x_sampling)
-  }
-  confint<-quantile(boot_mean, c(alpha/2, 1 - alpha/2))
-  boostrap<-list(alpha=alpha, mean=estimate, n_sample=R, boostrap_confidence=confint)
-  return(boostrap)
+Boostrap <- function(X=matrix(rnorm(20,2,4),10,4),y=matrix(rnorm(10,5,3),10,1),alpha =0.05,R =20, seed = NULL, ...){
+    #Calculate the mean
+    X<-as.matrix(X)
+    y<-as.matrix(y)
+    n <- nrow(X)
+    for (k in 1:R){
+      for(i in 1:nrow(X)) {
+        sample_X = X[sample(1:n, nrow(X), replace = TRUE), ]
+        sample_y=y[sample(1:n, nrow(y), replace = TRUE), ]
+      }
+      coeff[k]<-quantile(beta_hat(sample_X, SampleY), c(alpha/2, 1-alpha))
+    }
+    LOWERCONFIDENCEINTERVAL = mean(coeff[,1])
+    UPPERCONFIDENCEINTERVAL = mean(coeff[,2])
+    confidence<-cbind(LOWERCONFIDENCEINTERVAL,UPPERCONFIDENCEINTERVAL)
+    return(confidence)
 }
-
- 
-
-  
 
